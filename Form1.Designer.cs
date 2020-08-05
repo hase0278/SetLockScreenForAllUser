2563d54c07b84eac54119a6bb0e7e0c7e1590296
MyPC <MyPC@HASE>
2020-08-05 20:48:08 +0800

    Add .gitignore and .gitattributes.

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace SetLockScreenForAllUser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ImageView = new System.Windows.Forms.PictureBox();
+            this.SelectBtn = new System.Windows.Forms.Button();
+            this.SetLockScreenBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ImageView
+            // 
+            this.ImageView.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ImageView.Location = new System.Drawing.Point(25, 35);
+            this.ImageView.Name = "ImageView";
+            this.ImageView.Size = new System.Drawing.Size(580, 222);
+            this.ImageView.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ImageView.TabIndex = 0;
+            this.ImageView.TabStop = false;
+            // 
+            // SelectBtn
+            // 
+            this.SelectBtn.Location = new System.Drawing.Point(254, 263);
+            this.SelectBtn.Name = "SelectBtn";
+            this.SelectBtn.Size = new System.Drawing.Size(82, 23);
+            this.SelectBtn.TabIndex = 1;
+            this.SelectBtn.Text = "Select Image";
+            this.SelectBtn.UseVisualStyleBackColor = true;
+            this.SelectBtn.Click += new System.EventHandler(this.SelectBtn_Click);
+            // 
+            // SetLockScreenBtn
+            // 
+            this.SetLockScreenBtn.Location = new System.Drawing.Point(245, 292);
+            this.SetLockScreenBtn.Name = "SetLockScreenBtn";
+            this.SetLockScreenBtn.Size = new System.Drawing.Size(101, 23);
+            this.SetLockScreenBtn.TabIndex = 2;
+            this.SetLockScreenBtn.Text = "Set as lockscreen";
+            this.SetLockScreenBtn.UseVisualStyleBackColor = true;
+            this.SetLockScreenBtn.Visible = false;
+            this.SetLockScreenBtn.Click += new System.EventHandler(this.SetLockScreenBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(645, 337);
+            this.Controls.Add(this.SetLockScreenBtn);
+            this.Controls.Add(this.SelectBtn);
+            this.Controls.Add(this.ImageView);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Universal Lockscreen Setter";
+            ((System.ComponentModel.ISupportInitialize)(this.ImageView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ImageView;
+        private System.Windows.Forms.Button SelectBtn;
+        private System.Windows.Forms.Button SetLockScreenBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace SetLockScreenForAllUser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void setLockScreen(string imgPath)
+        {
+            RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP");
+
+            key.SetValue("LockScreenImagePath", @imgPath, RegistryValueKind.String);
+            key.SetValue("LockScreenImageUrl", @imgPath, RegistryValueKind.String);
+            key.SetValue("LockScreenImageStatus", 1, RegistryValueKind.DWord);
+            key.Close();
+
+            MessageBox.Show("Lockscreen set to "+ @imgPath +". Please restart your computer to see changes.", "Universal Lockscreen Setter");
+        }
+
+        private string getImagePath()
+        {
+            string path = "";
+
+            OpenFileDialog getPath = new OpenFileDialog();
+            getPath.Title = "Select your lockscreen:";
+            getPath.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+
+            if (getPath.ShowDialog() == DialogResult.OK)
+            {
+                path = getPath.FileName;
+            }
+
+            return path;
+        }
+
+        private void SelectBtn_Click(object sender, EventArgs e)
+        {
+            ImageView.ImageLocation = getImagePath();
+            SetLockScreenBtn.Visible = true;
+        }
+
+        private void SetLockScreenBtn_Click(object sender, EventArgs e)
+        {
+            setLockScreen(ImageView.ImageLocation);
+            ImageView.ImageLocation = "";
+            SetLockScreenBtn.Visible = false;
+        }
+    }
+}
